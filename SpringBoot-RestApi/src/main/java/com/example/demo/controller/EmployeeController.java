039d34d4e568a56e8c7b5fc2ab4c00f31517ac80
Sachin1708 <84273669+Sachin1708@users.noreply.github.com>
2021-05-21 02:07:03 -0700

    Add files via upload

---
+++ SpringBoot-RestApi/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepo;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+	@Autowired
+	EmployeeRepo empRepo;
+
+	@GetMapping("/employees")
+	public List<Employee> getEmployee() {
+		return empRepo.findAll();
+	}
+
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+			throws ResourceNotFoundException {
+		Employee employee = empRepo.findById(employeeId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+		return ResponseEntity.ok().body(employee);
+	}
+
+}
